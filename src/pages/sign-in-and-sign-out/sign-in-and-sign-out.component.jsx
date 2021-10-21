b5fc78bdb5934ea89fef861023159fcd0b95588b
= <thiago-in@hotmail.com>
2021-10-20 22:58:33 -0400

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from "../../components/sign-up/sign-up.component";
 import './sign-in-and-sign-out.styles.scss';
 
 const SignInAndSignOutPage = ()=> (
-    <div className='sing-in-and-sing-out'>
+    <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
