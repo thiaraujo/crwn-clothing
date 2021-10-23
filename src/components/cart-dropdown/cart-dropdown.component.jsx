5791d4de4e8a258c1e95d22e5735780913cd1ff2
= <thiago-in@hotmail.com>
2021-10-23 09:48:34 -0400

    added cartIcon and cart-dropdown components to our application, also implemented cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import CustonButton from "../custom-button/custon-button.component";
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ()=>(
+    <div className='cart-dropdown'>
+        <div className='cart-items' />
+        <CustonButton>GO TO CHECKOUT</CustonButton>
+    </div>
+);
+
+export default CartDropdown;
