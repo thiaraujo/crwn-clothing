b3360d4af353b6293dab934860dc29c0053dc095
= <thiago-in@hotmail.com>
2021-10-25 22:57:14 -0400

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -19,7 +19,9 @@ const CollectionItem = ({ item, addItem })=>{
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <CustomButton onClick={()=> addItem(item)} inverted>Add to Cart</CustomButton>
+            <CustomButton onClick={()=> addItem(item)} inverted>
+            Add to Cart
+            </CustomButton>
         </div>    
     );
 };
